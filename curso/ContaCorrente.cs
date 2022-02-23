ee3b47012dd0b4ec5dc9606eb95734603682b149
André Bessa <andrebessax@gmail.com>
2022-02-23 12:34:52 -0300

    Update

--- curso/ContaCorrente.cs
+++ curso/ContaCorrente.cs
@@ -6,25 +6,26 @@
         private int codigo_agencia;
         private string nome_agencia;
         private double saldo = 150;
-
-        private Cliente Titular { get; set; }
+        private string titular;
+             
         public static int TotalDeContasCriadas { get; private set; }
         public string Conta { get => conta; set => conta = value; }
         public int Codigo_agencia { get => codigo_agencia; set => codigo_agencia = value; }
         public string Nome_agencia { get => nome_agencia; set => nome_agencia = value; }
         public double Saldo { get => saldo; set => saldo = value; }
+        public string Titular { get => titular; set => titular = value; }
 
         ////Construtor padrão.
         public ContaCorrente()
         {
             TotalDeContasCriadas = TotalDeContasCriadas + 1;
         }
-        public ContaCorrente(string conta, int codigo_agencia, string nome_agencia, double saldo, Cliente titular)
+        public ContaCorrente(string conta, int codigo_agencia, string nome_agencia, double saldo,string titular)
         {
             this.Conta = conta;
             this.Codigo_agencia = codigo_agencia;
             this.Nome_agencia = nome_agencia;
-            this.Saldo = this.Saldo+ saldo;
+            this.Saldo = this.Saldo+ saldo;    
             this.Titular = titular;
             TotalDeContasCriadas = TotalDeContasCriadas + 1;
         }
@@ -71,9 +72,8 @@
             Console.WriteLine($"COD AGÊNCIA: {this.Codigo_agencia}");
             Console.WriteLine($"NOME AGÊNCIA: {this.Nome_agencia}");
             Console.WriteLine($"SALDO: {this.Saldo}");
-            Console.WriteLine($"TITULAR: {this.Titular.Nome}");
-            Console.WriteLine($"CPF: {this.Titular.Cpf}");
-            Console.WriteLine($"PROFISSÃO: {this.Titular.Profissao}");
+            Console.WriteLine($"TITULAR: {this.Titular}");
+
         }
     }
 }
--- curso/Program.cs
+++ curso/Program.cs
@@ -2,16 +2,19 @@
 
 Console.WriteLine("Bem vindo ao seu banco,  Bytebank ");
 
-Cliente titular1 = new Cliente("25035211002", "André Silva","Administrador");
-ContaCorrente conta1 = new ContaCorrente("012596-X",789,"Agencia Central",188,titular1);
-conta1.InformacaoConta();
+ContaCorrente conta1 = new ContaCorrente("012596-X",789,"Agencia Central",188,"André Silva");
+conta1.Sacar(30);
+conta1.Depositar(60);
+Console.WriteLine($"Saldo => {conta1.Saldo}");
 
-Console.WriteLine("Total de Contas Criadas: "+ContaCorrente.TotalDeContasCriadas);
+ContaCorrente conta2 = new ContaCorrente("5248616-X", 963, "Agencia Marataízes", 100, "Barbará Menezes");
+conta1.Sacar(96);
+conta1.Depositar(135);
+Console.WriteLine($"Saldo => {conta2.Saldo}");
 
-Cliente titular2 = new Cliente("23115799080", "Miguel Ostrander", "Roteirista");
-ContaCorrente conta2 = new ContaCorrente("5248616-X", 963, "Agencia Marataízes", 100, titular2);
-conta2.InformacaoConta();
-
-Console.WriteLine("Total de Contas Criadas: " + ContaCorrente.TotalDeContasCriadas);
+//Transferir
+conta1.Transferir(37, conta2);
+Console.WriteLine($"Saldo => {conta1.Saldo}");
+Console.WriteLine($"Saldo => {conta2.Saldo}");
 
 Console.ReadKey();
