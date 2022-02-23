d72195b8d713dd1ffb9b31d6d533092ec54a1aa3
André Bessa <andrebessax@gmail.com>
2022-02-23 13:41:56 -0300

    Update

--- curso/Program.cs
+++ curso/Program.cs
@@ -1,29 +1,6 @@
-﻿//Console.WriteLine("Bem vindo ao seu banco,  Bytebank ");
+﻿using bytebank;
 
-//string titular = "André Silva";
-//string conta = "101023-X";
-//string agencia = "Agência Central";
-//double saldo = 100;
-
-//Console.WriteLine($"Títular da conta: {titular}");
-//Console.WriteLine($"Número da conta: {conta}");
-//Console.WriteLine($"Agência: {agencia}");
-////Console.WriteLine($"Saldo R$ {saldo}");
-//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", saldo)}");
-
-//Console.WriteLine("===============================================");
-
-//string titular2 = "Selina Correa";
-//string conta2 = "365987-A";
-//string agencia2 = "Agência Bairro União";
-//double saldo2 = 375;
-
-//Console.WriteLine($"Títular da conta: {titular2}");
-//Console.WriteLine($"Número da conta: {conta2}");
-//Console.WriteLine($"Agência: {agencia2}");
-//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", saldo2)}");
-
-using bytebank;
+Console.WriteLine("Bem vindo ao seu banco,  Bytebank ");
 
 ContaCorrente conta = new ContaCorrente();
 conta.titular = "André Silva";
@@ -31,18 +8,31 @@ conta.conta = "12345-Y";
 conta.agencia = "Agência Rua dos Bancos";
 conta.saldo = 123.96;
 
-Console.WriteLine($"Títular da conta: {conta.titular}");
-Console.WriteLine($"Número da conta: {conta.conta}");
-Console.WriteLine($"Agência: {conta.agencia}");
-Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+//Console.WriteLine($"Títular da conta: {conta.titular}");
+//Console.WriteLine($"Número da conta: {conta.conta}");
+//Console.WriteLine($"Agência: {conta.agencia}");
+//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+
 
 ContaCorrente conta2 = new ContaCorrente();
-conta2.titular = "Amanda Silva";
-conta2.conta = "625433-Y";
-conta2.agencia = "Agência Rua 25 de Março";
-conta2.saldo = 5156.9;
+conta2.titular = "André Silva";
+conta2.conta = "12345-Y";
+conta2.agencia = "Agência Rua dos Bancos";
+conta2.saldo = 123.96;
 
-Console.WriteLine($"Títular da conta: {conta2.titular}");
-Console.WriteLine($"Número da conta: {conta2.conta}");
-Console.WriteLine($"Agência: {conta2.agencia}");
-Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta2.saldo)}");
+//Console.WriteLine($"Títular da conta: {conta2.titular}");
+//Console.WriteLine($"Número da conta: {conta2.conta}");
+//Console.WriteLine($"Agência: {conta2.agencia}");
+//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+
+Console.WriteLine(">>> Tipo por valor");
+double saldo2 = 123.96;
+double saldo3 = conta2.saldo;
+Console.WriteLine(saldo2== conta2.saldo);
+Console.WriteLine(saldo2 == saldo3);
+
+Console.WriteLine(">>> Tipo por referência");
+Console.WriteLine(conta == conta2);
+
+Console.WriteLine(conta.Equals(conta2));
+Console.ReadKey();
