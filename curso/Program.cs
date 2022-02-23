e7322a3c1c08ae6c8e3776116cb027018f8f3403
André Bessa <andrebessax@gmail.com>
2022-02-23 12:35:55 -0300

    Update

--- curso/Program.cs
+++ curso/Program.cs
@@ -2,16 +2,30 @@
 
 Console.WriteLine("Bem vindo ao seu banco,  Bytebank ");
 
-Cliente titular1 = new Cliente("25035211002", "André Silva","Administrador");
-ContaCorrente conta1 = new ContaCorrente("012596-X",789,"Agencia Central",188,titular1);
-conta1.InformacaoConta();
 
-Console.WriteLine("Total de Contas Criadas: "+ContaCorrente.TotalDeContasCriadas);
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "12345-Y";
+conta1.codigo_agencia = 123;
+conta1.nome_agencia = "Agência Rua dos Bancos";
+conta1.saldo = 123.96;
 
-Cliente titular2 = new Cliente("23115799080", "Miguel Ostrander", "Roteirista");
-ContaCorrente conta2 = new ContaCorrente("5248616-X", 963, "Agencia Marataízes", 100, titular2);
-conta2.InformacaoConta();
+Console.WriteLine($"Títular da conta: {conta1.titular}");
+Console.WriteLine($"Número da conta: {conta1.conta}");
+Console.WriteLine($"Agência: {conta1.codigo_agencia} - {conta1.nome_agencia}");
+Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta1.saldo)}");
 
-Console.WriteLine("Total de Contas Criadas: " + ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Amanda Silva";
+conta2.conta = "625433-Y";
+conta2.codigo_agencia = 321;
+conta2.nome_agencia = "Agência Rua 25 de Março";
+conta2.saldo = 5156.9;
+
+Console.WriteLine($"Títular da conta: {conta2.titular}");
+Console.WriteLine($"Número da conta: {conta2.conta}");
+Console.WriteLine($"Agência: {conta2.codigo_agencia} - {conta2.nome_agencia}");
+Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta2.saldo)}");
 
 Console.ReadKey();
