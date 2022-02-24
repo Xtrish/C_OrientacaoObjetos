bf8e426a6a38b98fdc8164144927110fb7a82ea2
André Bessa <andrebessax@gmail.com>
2022-02-24 14:55:14 -0300

    Update

--- curso/ContaCorrente.cs
+++ curso/ContaCorrente.cs
@@ -6,6 +6,19 @@
         public int codigo_agencia;
         public string nome_agencia;
         public double saldo = 150;
-        public string titular;        
+        public string titular;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+        }
+
+       
     }
 }
--- curso/Program.cs
+++ curso/Program.cs
@@ -16,16 +16,26 @@ Console.WriteLine($"Agência: {conta1.codigo_agencia} - {conta1.nome_agencia}");
 Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta1.saldo)}");
 
 
-ContaCorrente conta2 = new ContaCorrente();
-conta2.titular = "Amanda Silva";
-conta2.conta = "625433-Y";
-conta2.codigo_agencia = 321;
-conta2.nome_agencia = "Agência Rua 25 de Março";
-conta2.saldo = 5156.9;
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = "Amanda Silva";
+//conta2.conta = "625433-Y";
+//conta2.codigo_agencia = 321;
+//conta2.nome_agencia = "Agência Rua 25 de Março";
+//conta2.saldo = 5156.9;
 
-Console.WriteLine($"Títular da conta: {conta2.titular}");
-Console.WriteLine($"Número da conta: {conta2.conta}");
-Console.WriteLine($"Agência: {conta2.codigo_agencia} - {conta2.nome_agencia}");
-Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta2.saldo)}");
+//Console.WriteLine($"Títular da conta: {conta2.titular}");
+//Console.WriteLine($"Número da conta: {conta2.conta}");
+//Console.WriteLine($"Agência: {conta2.codigo_agencia} - {conta2.nome_agencia}");
+//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta2.saldo)}");
+
+//if (conta2.saldo >= 100)
+//{
+//    conta2.saldo -= 100;
+//}
+
+Console.WriteLine(conta1.saldo);
+bool resultadoSaque = conta1.Sacar(500);
+Console.WriteLine(conta1.saldo);
+Console.WriteLine(resultadoSaque);
 
 Console.ReadKey();
