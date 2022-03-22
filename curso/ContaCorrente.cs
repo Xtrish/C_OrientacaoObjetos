8b3d6ce4a423de42f87f3cb56f48fa11da4a215a
André Bessa <andrebessax@gmail.com>
2022-03-22 12:20:26 -0300

    Update: Ajustes

--- curso/ContaCorrente.cs
+++ curso/ContaCorrente.cs
@@ -1,10 +1,14 @@
-﻿namespace bytebank
+﻿using bytebank;
+
+namespace bytebank
 {
-    class ContaCorrente
+    public class ContaCorrente
     {
         public string titular;
         public string conta;
-        public string agencia;
+        public int numero_agencia;
+        public string nome_agencia;
         public double saldo;
+
     }
 }
--- curso/Program.cs
+++ curso/Program.cs
@@ -1,38 +1,21 @@
 ﻿using bytebank;
 
-Console.WriteLine("Bem vindo ao seu banco,  Bytebank ");
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!\n");
 
-ContaCorrente conta = new ContaCorrente();
-conta.titular = "André Silva";
-conta.conta = "12345-Y";
-conta.agencia = "Agência Rua dos Bancos";
-conta.saldo = 123.96;
-
-//Console.WriteLine($"Títular da conta: {conta.titular}");
-//Console.WriteLine($"Número da conta: {conta.conta}");
-//Console.WriteLine($"Agência: {conta.agencia}");
-//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
-
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "110123-X";
+conta1.numero_agencia = 123;
+conta1.nome_agencia = "Agência Central";
+conta1.saldo = 100;
 
 ContaCorrente conta2 = new ContaCorrente();
 conta2.titular = "André Silva";
-conta2.conta = "12345-Y";
-conta2.agencia = "Agência Rua dos Bancos";
-conta2.saldo = 123.96;
+conta2.conta = "10123-X";
+conta2.numero_agencia = 321;
+conta2.nome_agencia = "Agência Central";
+conta2.saldo = 100;
 
-//Console.WriteLine($"Títular da conta: {conta2.titular}");
-//Console.WriteLine($"Número da conta: {conta2.conta}");
-//Console.WriteLine($"Agência: {conta2.agencia}");
-//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+conta1 = conta2;
 
-Console.WriteLine(">>> Tipo por valor");
-double saldo2 = 123.96;
-double saldo3 = conta2.saldo;
-Console.WriteLine(saldo2== conta2.saldo);
-Console.WriteLine(saldo2 == saldo3);
-
-Console.WriteLine(">>> Tipo por referência");
-Console.WriteLine(conta == conta2);
-
-Console.WriteLine(conta.Equals(conta2));
-Console.ReadKey();
+Console.WriteLine(conta1==conta2);
